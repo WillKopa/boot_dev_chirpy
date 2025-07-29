e8841000fcacfb70ee2764dc9d53d71fadd012b2
William Kopaczewski <will.kopaczewski@gmail.com>
2025-07-29 01:12:21 -0400

    Updated create user to require a password

--- api/handler_create_user.go
+++ api/handler_create_user.go
@@ -1,19 +1,50 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/WillKopa/boot_dev_chirpy/internal/auth"
+	"github.com/WillKopa/boot_dev_chirpy/internal/database"
+)
 
 func (cfg *apiConfig) create_user(rw http.ResponseWriter, req *http.Request) {
-	user, err := unmarshal_json[User](req)
+	type User_request struct {
+		Password	string	`json:"password"`
+		Email		string	`json:"email"`
+	}
+	
+	user_request, err := unmarshal_json[User_request](req)
 	if err != nil {
 		respond_with_error(rw, http.StatusBadRequest, "unable to parse json from request")
 		return
 	}
-	db_user, err := cfg.db_queries.CreateUser(req.Context(), user.Email)
+	if len(user_request.Password) < 1 {
+		respond_with_error(rw, http.StatusBadRequest, fmt.Sprintf("password is too short: must be at least %d characters long", 1))
+	}
+
+	hashed_password, err := auth.HashPassword(user_request.Password)
+	if err != nil {
+		respond_with_error(rw, http.StatusInternalServerError, "error saving password")
+		return
+	}
+
+	db_params := database.CreateUserParams {
+		Email: user_request.Email,
+		HashedPassword: hashed_password,
+	}
+
+	db_user, err := cfg.db_queries.CreateUser(req.Context(), db_params)
 
 	if err != nil {
 		respond_with_error(rw, http.StatusInternalServerError, "Something went wrong, user not created")
 		return
 	}
 
-	respond_with_json(rw, http.StatusCreated, User(db_user))
+	respond_with_json(rw, http.StatusCreated, User{
+		ID: db_user.ID,
+		CreatedAt: db_user.CreatedAt,
+		UpdatedAt: db_user.UpdatedAt,
+		Email: db_params.Email,
+	})
 }
--- api/response_structs.go
+++ api/response_structs.go
@@ -5,14 +5,12 @@ import (
 
 	"github.com/google/uuid"
 )
-// These structs will match the generated database structs of the same name.
-// They are used to set the json metadata by casting the generated structs to these.
 
 type User struct {
-	ID			uuid.UUID	`json:"id"`
-	CreatedAt	time.Time	`json:"created_at"`
-	UpdatedAt	time.Time	`json:"updated_at"`
-	Email		string		`json:"email"`	
+	ID				uuid.UUID	`json:"id"`
+	CreatedAt		time.Time	`json:"created_at"`
+	UpdatedAt		time.Time	`json:"updated_at"`
+	Email			string		`json:"email"`
 }
 
 type Chirp struct {
