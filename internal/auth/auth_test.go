dcb97a2ae3cc73aec51bfe84fb4d6d752c21f5ce
William Kopaczewski <will.kopaczewski@gmail.com>
2025-07-29 16:16:51 -0400

    improved auth tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,31 +2,50 @@ package auth
 
 import (
 	"testing"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
 func TestHashPassword(t *testing.T) {
-	password := "password"
-	result, _ := HashPassword(password)
+	password1 := "password"
+	password2 := "electricboogaloo"
+	hash1, _:= HashPassword(password1)
 
-	err := bcrypt.CompareHashAndPassword([]byte(result), []byte(password)) 
-	if err != nil {
-		t.Errorf("hashed password is not equal to the given password")
+	tests := []struct{
+		name			string
+		password		string
+		hash 			string
+		want_err		bool
+	}{
+		{
+			name: 			"Correct Password",
+			password: 		password1,
+			hash: 			hash1,
+			want_err: 		false,
+		},
+		{
+			name: 			"Inorrect Password",
+			password: 		password2,
+			hash: 			hash1,
+			want_err: 		true,
+		},
+		{
+			name: 			"Empty Password",
+			password: 		"",
+			hash: 			hash1,
+			want_err: 		true,
+		},{
+			name: 			"Invalid Hash",
+			password: 		password1,
+			hash: 			"hash",
+			want_err: 		true,
+		},
 	}
-}
 
-func TestCheckPasswordHash(t *testing.T) {
-	password := "password"
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-
-	if err != nil {
-		t.Fatalf("error hashing password: %s", err)
-	}
-
-	err = CheckPasswordHash(password, string(hash))
-
-	if err != nil {
-		t.Errorf("hash checker is not working correctly")
+	for _, hash_test := range tests {
+		t.Run(hash_test.name, func (t *testing.T) {
+			err := CheckPasswordHash(hash_test.password, hash_test.hash)
+			if (err != nil) && !hash_test.want_err {
+				t.Errorf("CheckPasswordHash() error = %v, wanted error: %v", err, hash_test.want_err)
+			}
+		})
 	}
 }
--- internal/auth/jwt_test.go
+++ internal/auth/jwt_test.go
@@ -7,20 +7,58 @@ import (
 	"github.com/google/uuid"
 )
 
-func TestMakeJWT(t *testing.T) {
+func TestJWT(t *testing.T) {
 	user_id := uuid.New()
 	token_secret := "it's a secret to everybody"
 	expires_in := 5 * time.Minute
 	result, err := MakeJWT(user_id, token_secret, expires_in)
+
 	if err != nil {
 		t.Fatalf("error making jwt token: %s", err)
 	}
-	result_user_id, err := ValidateJWT(result, token_secret)
-	if err != nil {
-		t.Fatalf("error decoding jwt token: %s", err)
+
+	tests := []struct {
+		name				string
+		token_string		string
+		token_secret		string
+		expected_user_ud	uuid.UUID
+		want_err			bool
+	}{
+		{
+			name: 				"Valid token",
+			token_string: 		result,
+			token_secret: 		token_secret,
+			expected_user_ud: 	user_id,
+			want_err:  			false,
+		},
+		{
+			name: 				"Invalid token",
+			token_string: 		"not valid",
+			token_secret: 		token_secret,
+			expected_user_ud: 	uuid.Nil,
+			want_err:  			true,
+		},
+		{
+			name: 				"Wrong secret",
+			token_string: 		result,
+			token_secret: 		token_secret + "more secret",
+			expected_user_ud: 	uuid.Nil,
+			want_err:  			true,
+		},
 	}
 
-	if result_user_id != user_id {
-		t.Errorf("user id and decoded user id are not the same")
+	for _, token_test := range tests {
+		t.Run(token_test.name, func(t *testing.T) {
+			result_user_id, err := ValidateJWT(token_test.token_string, token_test.token_secret)
+			
+			if (err != nil) && !token_test.want_err {
+				t.Errorf("error decoding jwt token: %s", err)
+				return
+			}
+
+			if result_user_id != token_test.expected_user_ud {
+				t.Errorf("user id and decoded user id are not the same")
+			}
+		})
 	}
 }
