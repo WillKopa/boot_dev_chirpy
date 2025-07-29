2821aa91a6ef0c541970d41758a8d9c42efc06e0
William Kopaczewski <will.kopaczewski@gmail.com>
2025-07-29 18:36:18 -0400

    Added function to get the jwt token from the header

--- internal/auth/jwt_test.go
+++ internal/auth/jwt_test.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"net/http"
 	"testing"
 	"time"
 
@@ -58,6 +59,55 @@ func TestJWT(t *testing.T) {
 
 			if result_user_id != token_test.expected_user_ud {
 				t.Errorf("user id and decoded user id are not the same")
+			}
+		})
+	}
+}
+
+func TestGetBearerToken(t *testing.T) {
+	token := "frodo"
+	prefix := "Bearer "
+
+	header1 := http.Header{}
+	header1.Set("Authorization", prefix + token)	
+
+	no_prefix_header := http.Header{}
+	no_prefix_header.Set("Authorization", token)
+
+	tests := []struct{
+		name				string
+		expected_token		string
+		header				http.Header
+		expect_error		bool
+	}{
+		{
+			name: 			"Header with correct syntax",
+			expected_token: "frodo",
+			header:   		header1,
+			expect_error: 	false,
+		},
+		{
+			name: 			"Header with no auth",
+			expected_token: "",
+			header:    		http.Header{},
+			expect_error: 	true,
+		},
+		{
+			name:			"Header without Bearer",
+			expected_token: "",
+			header:    		no_prefix_header,
+			expect_error: 	true,
+		},
+	}
+
+	for _, token_test := range tests {
+		t.Run(token_test.name, func(t *testing.T) {
+			result, err := GetBearerToken(token_test.header)
+			if (err != nil) && !token_test.expect_error {
+				t.Errorf("error calling GetBearerToken %v", err)
+			}
+			if (result != token_test.expected_token) {
+				t.Errorf("error calling GetBearerToken. Got %s, but expected %s", result, token_test.expected_token)
 			}
 		})
 	}
